b2d4585de765df3adc525cc8171d3eed9344045b
gr1101 <gauravrai110@gmail.com>
2025-01-25 01:08:23 +0530

    second commit

--- src/mypkg/Mycalc.java
+++ src/mypkg/Mycalc.java
@@ -6,9 +6,16 @@ public class Mycalc {
 		return a+b;
 	}
 	
+	int diff(int a, int b) {
+		return a-b;
+	}
+
+	
 	public static void main(String[] args) {
 		Mycalc calc = new Mycalc();
 		System.out.println("Sum is "+calc.sum(20, 10));
+		System.out.println("Sum is "+calc.diff(20, 10));
+		
 	}
 
 }
