4dc6a57840fcc3846e4e560ab9fb42a48c061f73
gr1101 <gauravrai110@gmail.com>
2025-01-25 00:52:17 +0530

    first commit

---
+++ src/mypkg/Mycalc.java
@@ -0,0 +1,14 @@
+package mypkg;
+
+public class Mycalc {
+	
+	int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		Mycalc calc = new Mycalc();
+		System.out.println("Sum is "+calc.sum(20, 10));
+	}
+
+}
